944470158f2d638f963f3a19d4cdb73910f415ee
Brendon J. Brewer <brendon.brewer@gmail.com>
2021-03-25 16:00:34 +1300

    Finishing class!

--- subdir/gp.R
+++ subdir/gp.R
@@ -7,8 +7,16 @@ C = matrix(nrow=N, ncol=N)
 
 C = exp(-0.5*(row(C) - col(C))^2 / 100^2)
 
-pdf("covariance_matrix.pdf")
-image(C)
-dev.off()
+# For numerical stability!
+C = C + 1E-4*diag(N)
 
+#pdf("covariance_matrix.pdf")
+#image(C)
+#dev.off()
+
+U = chol(C)
+n = rnorm(1000)
+y = t(U) %*% n
+
+plot(y, type="l")
 
